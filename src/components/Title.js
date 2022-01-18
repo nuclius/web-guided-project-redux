293351b6e31bfc3ae8e7655d7a2cf0a1625be5a7
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-18 13:21:22 -0500

    lecture work-in-progress

--- src/components/Title.js
+++ src/components/Title.js
@@ -9,8 +9,7 @@ import TitleDisplay from './TitleDisplay';
 import TitleForm from './TitleForm';
 
 const Title = (props) => {
-  // const [state, dispatch] = useReducer(titleReducer, initialState);
-
+  console.log(props);
   const handleToggleEditing = () => {
     // dispatch(toggleEditing());
   }
@@ -38,7 +37,9 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(Title);
+// export default connect(mapStateToProps)(Title);
+export default Title;
+
 //mapStateToProps : function that gets the current state and injects into props anything that is returned.
 //mapActionToProps : object that contains action creators. These functions are injected into props. They are AUTOMATICALLY DISPATCHED, when executed.
 //component : the component we are injecting props into.
