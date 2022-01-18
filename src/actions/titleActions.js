ddc9a3a5dda8169922e3ded62324bdcf0c0e3191
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-18 13:23:55 -0500

    lecture work-in-progress

--- src/actions/titleActions.js
+++ src/actions/titleActions.js
@@ -2,6 +2,7 @@ export const TOGGLE_EDITING = "TOGGLE_EDITING";
 export const UPDATE_TITLE = "UPDATE_TITLE";
 
 export function toggleEditing() {
+    console.log("2. Return the action object.");
     return { type: TOGGLE_EDITING };
 }
 
